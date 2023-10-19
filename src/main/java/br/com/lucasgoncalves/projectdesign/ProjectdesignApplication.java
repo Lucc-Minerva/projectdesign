3e3bf9a2c8106071d831efaf696b4d05e0720f46
lucas <lucaston2323@gmail.com>
2023-10-19 01:47:25 -0300

    first commit

---
+++ src/main/java/br/com/lucasgoncalves/projectdesign/ProjectdesignApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucasgoncalves.projectdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectdesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectdesignApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucasgoncalves/projectdesign/ProjectdesignApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasgoncalves.projectdesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectdesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
